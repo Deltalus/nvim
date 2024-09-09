9af1949dce2d1296c50a62603bf860bd23a555e1
Emilio Espinosa <eesp95@gmail.com>
2024-09-09 22:09:07 +0900

    Added TS LSP

--- lua/deltalus/plugins/colorschemes/catpuccin.lua
+++ lua/deltalus/plugins/colorschemes/catpuccin.lua
@@ -4,7 +4,8 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			style = "mocha",
+			flavour = "mocha",
+			transparent_background = true,
 		})
 
 		vim.cmd([[colorscheme catppuccin]])
